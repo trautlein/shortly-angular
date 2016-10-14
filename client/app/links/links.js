eaaf544b22ef1ff60eeedd6d2134e6db00d41c5a
trautlein <hans@trautlein.com>
2016-10-14 15:45:34 -0700

    Passing all LinksController tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,10 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  Links.getAll().then(function(data) {
+    $scope.data.links = data;
+  });
+
 });
