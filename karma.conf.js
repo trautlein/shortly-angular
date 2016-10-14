0e1ded2aa0022465b6762a351376f69f324b6cf8
trautlein <hans@trautlein.com>
2016-10-14 15:23:59 -0700

    Change from unicorn and nyan to mocha style reporter

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     ],
 
     // test results reporter to use
-    reporters: ['nyan', 'unicorn'],
+    reporters: ['mocha'],
 
     // start these browsers. PhantomJS will load up in the background
     browsers: ['PhantomJS'],
